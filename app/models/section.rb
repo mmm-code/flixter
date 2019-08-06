48e2f0603bc9b967cc5b7f9e0e4b7f5a9f5a7b04
Barbara Sanders <barbarasanderstruth@gmail.com>
2019-08-06 22:27:56 +0000

    L18

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,7 @@
 class Section < ApplicationRecord
   belongs_to :course
-  has_many :sections
+  #has_many :sections
+  has_many :lessons
   
   
   validates :title, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   resources :courses, only: [:index, :show]
   namespace :instructor do
+    #resources :courses, only: [:new, :create, :show] do
+      #resources :sections, only: [:new, :create] do
+        #resources :lessons, only: [:new, :create]
+    # L18 reroute
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
     end
